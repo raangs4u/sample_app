dc59060bd9e389d9cd63d5c909492a98de82da28
ranga.babu <raangs4u@gmail.com>
2013-05-20 10:42:14 +0530

    Add a StaticPages controller

---
+++ generate/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '5a5f26a1a2475dbf9ed9a61d454f13ad88b9e8bbe41fdb379dd308825af4937e6df4b14948fcfdbb55e67055688b3a370b1780dc2ed7a73db164f2dc72086f2e';
---
+++ generate/config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_generate_session',
+  :secret      => '0792a26491eca107d4f38eef94e9cac0662b5d741bb4ae612f849589272254db764250dd25cc2da538f5b28ad29b254d9c37635b8a7747026d0e76007809fda9'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
